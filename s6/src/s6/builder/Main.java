b1b1102ab595a51cf5ad84ec75ec435f4fef8b58
VisanLarisa <82821372+VisanLarisa@users.noreply.github.com>
2022-04-05 15:22:12 +0300

    Add files via upload

---
+++ s6/src/s6/builder/Main.java
@@ -0,0 +1,13 @@
+package s6.builder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//versiunea constructori (gresita)
+		User user1=new User("larisa", "larisa20", "lrs@gmail.com", "Belved V.", "0000000000", "1 jan");
+		User user2=new User("larisa", "larisa20", "lrs@gmail.com", "1 jan");
+		//User user3=new User("larisa", "larisa20", "lrs@gmail.com", "00000000");	//poate lua orice constructor cu nr de parametrii
+		
+	}
+}
